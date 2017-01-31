0f23c9249e7963429bd0873b7e03a993457ebc06
Sudarshan <bsudarshan@outlook.com>
2017-01-31 02:22:07 -0500

    I3: game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+@@secret_number = rand(100)
+@@guesses_left = 5
+
+def check_guess(guess)
+  @@guesses_left -= 1
+  if @@guesses_left != 0
+    if guess > @@secret_number
+      "#{guess} is too high."
+    elsif guess < @@secret_number
+      "#{guess} is too low."
+    else
+      @@secret_number = rand(100)
+      @@guesses_left = 6
+      "perfect, #{guess} is correct."
+    end
+  else
+    @@secret_number = rand(100)
+    @@guesses_left = 5
+    "New game"
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess.to_i)
+  erb :index, :locals => {:guesses_left => @@guesses_left, :message => message}
 end
